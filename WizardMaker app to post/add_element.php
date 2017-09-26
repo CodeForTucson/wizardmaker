e6aed62f42373d974ac286e2753db1b16b323e63
Paul Tynan <paultynan89@gmail.com>
2017-09-26 15:38:33 -0700

    Issue 13. add_element now matches the spec.
    Fixed the button tool bar and added descriptions for each type of element.

--- WizardMaker app to post/add_element.php
+++ WizardMaker app to post/add_element.php
@@ -7,9 +7,14 @@ define('WIZTITLE', 'Element');
 define('BUTTON_1', '<a href="add_step.php" class="btn btn-primary" role="button">
          			<span class="glyphicon glyphicon-chevron-left"></span>The Step
          			</a>');
-define('BUTTON_2', '');
+define('BUTTON_2', '<a href="" class="btn btn-primary" role="button">
+         			Help
+         			</a>');
 define('BUTTON_3', '');
-define('BUTTON_4', '<a href="add_step.php" class="btn btn-primary" role="button">
+define('BUTTON_4', '');
+define('BUTTON_5', '');
+define('BUTTON_6', '');
+define('BUTTON_7', '<a href="add_step.php" class="btn btn-primary" role="button">
     				Cancel</span>
     				</a>');
 // Include the header:
@@ -23,16 +28,37 @@ function setAndGo(file) {
     window.location.assign("http://betterstuffbetterlife.com/pttrot/WizardMakerApp/" + file);
 }
 </script>
+
+<h3> Select the element you want to add to this step. </h3>
+<!--  create a row for each type of element and add a description -->
+<div class="row">
+	<div class="col-xs-4">
+		<button class="btn-info" onclick="setAndGo(\'text_element.php\')">Text</button>
+	</div> 
+	<div class="col-xs-6">
+		<p> Add a block of text. This may include calculations based on 
+		information you get from the user by using Ask for Input.</p>
+	</div>
+</div>
+<div class="row">
+	<div class="col-xs-4">
+		<button class="btn-info" onclick="setAndGo(\'image_element.php\')">Picture or Video</button>
+	</div> 
+	<div class="col-xs-6">
+		<p>Add a picture or video clip.  You can take it from this device if it has a camera
+		or upload it from your computer. </p>
+	</div>
+</div>
+<div class="row">
+	<div class="col-xs-4">
+		<button class="btn-info" onclick="setAndGo(\'askInput_element.php\')">Ask for Input</button>
+	</div> 
+	<div class="col-xs-6">
+		<p>Ask your user for a number that can be used with text to show calculations.</p>
+	</div>
+</div>
+		
 <?php
-
-print '<h3> Select the element you want to add to this step. </h3>';
-
-print '<button class="btn-info" onclick="setAndGo(\'text_element.php\')">Text</button><br>'; 
-print '<br>';
-print '<button class="btn-info" onclick="setAndGo(\'image_element.php\')">Picture or Video</button><br>';
-print '<br>';
-print '<button class="btn-info" onclick="setAndGo(\'askInput_element.php\')">Ask for Input</button><br>';
-print '<br>';
-// footer material
+// footer commands and material
 include 'templates/footer.html'; // Include the footer.
 ?>
